6663373d56fce30a3dc42d763f77bbd9f2ea1af1
vittpol <vittoriaanna.polieri@marconibari.edu.it>
2024-09-30 12:44:51 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    float n1, n2, n3, max ;
+    cout<<"inserisci il primo valore";
+    cin>>n1
+    cout<<"inserisci il secondo valore";
+    cin>>n2
+    cout<<"inserisci il terzo valore"
+    cin>>n3
+    if(n1>n2)
+    {
+        n1>n3
+        {
+            max=n1;
+        }
+        else
+        {
+            max=n3;
+        }
+    }
+    else
+    {
+        n2>n3
+        {
+            max=n2
+        }
+        else
+        {
+            max=n3
+        }
+    }
+    cout<<"il numero massimo è"<<max ;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
